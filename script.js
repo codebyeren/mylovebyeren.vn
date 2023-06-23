c809087efb7972b4c5fc8f27139b00cf9bffe317
Eren <115102495+codebyeren@users.noreply.github.com>
2023-06-23 23:52:48 +0700

    upload

--- script.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function() {
-    document.body.innerHTML = `
-        <h1>Trang web đã hoạt động trở lại!</h1>
-        <p>Cảm ơn bạn đã chờ đợi. Bạn có thể trải nghiệm những thay đổi mới của chúng tôi ngay bây giờ.</p>
-    `;
-}, 1000000000000000000000); // Thời gian (ms) để đoạn mã JavaScript này thực hiện sau khi tải trang
